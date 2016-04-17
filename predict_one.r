76f3820778afa1e1a79c7a3703c97d8c70a7dc43
grownOutIntern <cooldude7_vinay@yahoo.co.in>
2016-04-17 14:50:10 +0530

    made a file that can be used to predict a manually captured image of a number

---
+++ predict_one.r
@@ -0,0 +1,39 @@
+resize <- function(m){
+	d <- dim(m)
+	x <- matrix(nrow=28,ncol=28)
+	rx <- floor(d[1]/28)
+	cx <- floor(d[2]/28)
+	for(i in 1:28)
+	{
+		for(j in 1:28)
+		{
+			x[i,j]=0
+			for(k in 1:rx)
+			{
+				for(l in 1:cx)
+				{
+					x[i,j] = x[i,j] + m[(i-1)*rx + k,(j-1)*cx + l]
+				}
+			}
+		}
+	}
+	x <- x
+}
+
+rotate <- function(x) t(apply(x, 2, rev))
+
+library('jpeg')
+
+my_image <- readJPEG('two.jpg')
+
+r_val <- my_image[,,1]
+g_val <- my_image[,,2]
+b_val <- my_image[,,3]
+
+grayscale <- round(((r_val + g_val + b_val)/3)*255)
+
+im <- rotate(grayscale)
+
+im <- resize(im)
+
+image(im)
