b096653e2b82018535b7db9ed73e77b7673edf28
khuranavinay <khuranavinay76@gmail.com>
2016-04-27 13:20:14 +0530

    modified test.r

--- test.r
+++ test.r
@@ -1,15 +1,44 @@
 library('neuralnet')
 
-train <- read.csv('../train.csv',header=TRUE)
+train <- read.csv("../train.csv", header=TRUE)
 
-f = 'label~pixel0'
-for(i in 1:783)
+r <- sample(1:nrow(train),5)
+train1 <- train[r, ]
+labels <- as.numeric(train1[,1])
+train1 <- train1[,-1]
+
+res <- mat.or.vec(5,10)
+
+for(row in 1:nrow(res)) {
+    #Check for ith example
+    res[row, (labels[row]+1)] <- 1
+    }
+
+dimnames(res) <- list(NULL,colnames(res, do.NULL = FALSE, prefix = "label"))
+
+res <- as.data.frame(res)
+
+train1 <- merge(res,train1)
+
+
+f1 = ''
+for(i in 1:10)
 {
-	f <- paste(f,paste0('pixel',i,collapse=NULL),sep='+',collapse=NULL)
+	f1 <- paste(f1,paste0('label',i,collapse=NULL),sep='+',collapse=NULL)
 }
+f2 = ''
+for(i in 0:783)
+{
+	f2 <- paste(f2,paste0('pixel',i,collapse=NULL),sep='+',collapse=NULL)
+}
+f <- paste(f1,f2,sep='~',collapse=NULL)
 f <- as.formula(f)
 #print(f)
 
-net <- neuralnet(f, data=train, hidden=c(5,5))
+#print(train1)
+
+net <- neuralnet(f, data=train1, hidden=c(6), err.fct = 'ce', linear.output = FALSE, threshold = 0.1)
 
 print(net)
+
+plot(net)
